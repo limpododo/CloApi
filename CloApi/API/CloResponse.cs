60bfe86888f80e9cb3ac1a62064e8bf4ddae3e5e
limpododo <mxxm994@gmail.com>
2023-06-01 01:31:19 +0300

    Добавлены свойства объекта CloResponse. Results для ответа в виде массива объектов, Result для ответа в виде одного объекта

--- CloApi/API/CloResponse.cs
+++ CloApi/API/CloResponse.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.API
 {
-    public class CloResponse
+    public class CloResponse<T>
     {
+        [JsonPropertyName("code")]
+        public int? Code { get; set; }
+
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<T>? Results { get; set; }
+
+        [JsonPropertyName("result")]
+        public T? Result { get; set; }
     }
 }
