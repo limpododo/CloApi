b78f69028ce9518949b1996b908b356d1daac90f
limpododo <mxxm994@gmail.com>
2023-06-01 00:03:07 +0300

    Добавлены свойства объекту OperationSystem

--- CloApi/Infrastructure/ProjectObjects/OperationSystem.cs
+++ CloApi/Infrastructure/ProjectObjects/OperationSystem.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.ProjectObjects
 {
-    public class OperationSystem
+    public record class OperationSystem
     {
+        /// <summary>
+        /// Версия операционной системы
+        /// </summary>
+        [JsonPropertyName("version")]
+        public string? Version { get; set; }
+
+        /// <summary>
+        /// Название дистрибутива
+        /// </summary>
+        [JsonPropertyName("distribution")]
+        public string? Distribution { get; set; }
+
+        /// <summary>
+        /// Семейство операционных систем
+        /// </summary>
+        [JsonPropertyName("os_family")]
+        public string? OsFamily { get; set; }
     }
 }
