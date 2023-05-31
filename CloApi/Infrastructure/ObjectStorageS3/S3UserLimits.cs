5e4e8a378487f09ce4ea3121234839bd2bd10bd1
limpododo <mxxm994@gmail.com>
2023-05-31 23:50:50 +0300

    Добавлены свойства объекту S3UserLimits

--- CloApi/Infrastructure/ObjectStorageS3/S3UserLimits.cs
+++ CloApi/Infrastructure/ObjectStorageS3/S3UserLimits.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.ObjectStorageS3
 {
-    public class S3UserLimits
+    public record class S3UserLimits
     {
+        /// <summary>
+        /// Максимально допустимое количество объектов
+        /// </summary>
+        [JsonPropertyName("max_objects")]
+        public int? MaxObjects { get; set; }
+
+        /// <summary>
+        /// Максимально допустимый объем данных в Кб
+        /// </summary>
+        [JsonPropertyName("max_size")]
+        public int? MaxSize{ get; set; }
+
+        /// <summary>
+        /// Тип лимита пользователя - для каждого бакета, либо общая для пользователя S3
+        /// </summary>
+        [JsonPropertyName("type")]
+        public string? Type { get; set; }
     }
 }
