9efc0d320f4a60bd386f829cdec8604f989ee0c2
limpododo <mxxm994@gmail.com>
2023-05-31 23:35:37 +0300

    Добавлены свойства объекту Keypair

--- CloApi/Infrastructure/Network/Keypair.cs
+++ CloApi/Infrastructure/Network/Keypair.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.Network
 {
-    public class Keypair
+    public record class Keypair
     {
+        /// <summary>
+        /// Уникальный идентификатор SSH-ключа
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        /// <summary>
+        /// Имя SSH-ключа
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Значение публичного ключа
+        /// </summary>
+        [JsonPropertyName("public_key")]
+        public string? PublicKey { get; set; }
     }
 }
