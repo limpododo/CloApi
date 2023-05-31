ad004b5fdd534ff97f78125227ebc49a321a0c7d
limpododo <mxxm994@gmail.com>
2023-06-01 01:28:06 +0300

    Рефакторинг ProjectLimit. Свойства null (data_type?)

--- CloApi/Infrastructure/ProjectObjects/ProjectLimit.cs
+++ CloApi/Infrastructure/ProjectObjects/ProjectLimit.cs
@@ -8,24 +8,24 @@ namespace CloApi.Infrastructure.ProjectObjects
         /// Верхнее ограничение по использованию ресурса
         /// </summary>
         [JsonPropertyName("max")]
-        public int Max { get; set; }
+        public int? Max { get; set; }
 
         /// <summary>
         /// Единица измерения
         /// </summary>
         [JsonPropertyName("measure")]
-        public string Measure { get; set; }
+        public string? Measure { get; set; }
 
         /// <summary>
         /// Наименование ресурса
         /// </summary>
         [JsonPropertyName("name")]
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         /// <summary>
         /// Количество использованного ресурса
         /// </summary>
         [JsonPropertyName("used")]
-        public int Used { get; set; }
+        public int? Used { get; set; }
     }
 }
