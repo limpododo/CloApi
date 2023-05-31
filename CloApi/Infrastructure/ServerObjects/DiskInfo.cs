853768b87afc31f4faa5280510dbb8adb18e9bfa
limpododo <mxxm994@gmail.com>
2023-06-01 00:18:59 +0300

    Добавлены свойства объекту DiskInfo

--- CloApi/Infrastructure/ServerObjects/DiskInfo.cs
+++ CloApi/Infrastructure/ServerObjects/DiskInfo.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using CloApi.Infrastructure.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.ServerObjects
 {
-    public class DiskInfo
+    public record class DiskInfo
     {
+        /// <summary>
+        /// Уникальный идентификатор диска
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        /// <summary>
+        /// Тип диска. volume - для сетевых дисков, local - для локальных
+        /// </summary>
+        [JsonPropertyName("storage_type")]
+        public string? StorageType { get; set; }
+
+        /// <summary>
+        /// Список ссылок на детальную информацию о диске
+        /// </summary>
+        [JsonPropertyName("links")]
+        public List<Link>? Links { get; set; }
     }
 }
