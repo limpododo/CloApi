08f0c24e405ebd9b66545f4a7ae832431df48d23
limpododo <mxxm994@gmail.com>
2023-06-01 00:20:03 +0300

    Добавлены свойства объекту Flavor

--- CloApi/Infrastructure/ServerObjects/Flavor.cs
+++ CloApi/Infrastructure/ServerObjects/Flavor.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.ServerObjects
 {
-    public class Flavor
+    public record class Flavor
     {
+        /// <summary>
+        /// Количество RAM
+        /// </summary>
+        [JsonPropertyName("ram")]
+        public int? Ram { get; set; }
+
+        /// <summary>
+        /// Количество vCPU
+        /// </summary>
+        [JsonPropertyName("vcpus")]
+        public int? VCPUs { get; set; }
     }
 }
