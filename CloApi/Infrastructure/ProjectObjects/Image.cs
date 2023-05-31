d4a6f0c5f5b9756bc1e1b72667ecc9b6d9c60fc6
limpododo <mxxm994@gmail.com>
2023-05-31 23:57:49 +0300

    Добавлены свойства объекту Image

--- CloApi/Infrastructure/ProjectObjects/Image.cs
+++ CloApi/Infrastructure/ProjectObjects/Image.cs
@@ -2,11 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.ProjectObjects
 {
-    public class Image
+    public record class Image
     {
+        /// <summary>
+        /// Уникальный идентификатор образа
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        /// <summary>
+        /// Наименование образа
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Объект операционной системы с параметрами
+        /// </summary>
+        [JsonPropertyName("operation_system")]
+        public OperationSystem? OperationSystem { get; set; }
     }
 }
