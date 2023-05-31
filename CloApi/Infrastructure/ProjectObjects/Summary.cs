85b9dfbe57cc81cc3098878351411953ce5284e7
limpododo <mxxm994@gmail.com>
2023-06-01 00:15:53 +0300

    Добавлены свойства объекту Summary

--- CloApi/Infrastructure/ProjectObjects/Summary.cs
+++ CloApi/Infrastructure/ProjectObjects/Summary.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.ProjectObjects
 {
-    public class Summary
+    public record class Summary
     {
+        /// <summary>
+        /// Количество используемых плавающих IP адресов
+        /// </summary>
+        [JsonPropertyName("floating_ips")]
+        public int? FloatingIps { get; set; }
+
+        /// <summary>
+        /// Количество используемых сетей
+        /// </summary>
+        [JsonPropertyName("networks")]
+        public int? Networks { get; set; }
+
+        /// <summary>
+        /// Количество используемых серверов
+        /// </summary>
+        [JsonPropertyName("servers")]
+        public int? Servers { get; set; }
+
+        /// <summary>
+        /// Количество используемых дисков
+        /// </summary>
+        [JsonPropertyName("volumes")]
+        public int? Volumes { get; set; }
     }
 }
