8adb1675bc2924545f7852e6b417b61b7f7e401b
limpododo <mxxm994@gmail.com>
2023-05-31 23:37:34 +0300

    Добавлены свойства объекту Link

--- CloApi/Infrastructure/Network/Link.cs
+++ CloApi/Infrastructure/Network/Link.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.Network
 {
     public class Link
     {
+        /// <summary>
+        /// Ссылка
+        /// </summary>
+        [JsonPropertyName("href")]
+        public string? Href { get; set; }
+
+        /// <summary>
+        /// Версия API
+        /// </summary>
+        [JsonPropertyName("rel")] 
+        public string? ApiVersion { get; set; }
     }
 }
