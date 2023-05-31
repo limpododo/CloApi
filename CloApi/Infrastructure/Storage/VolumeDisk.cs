fc2bf475d7b994220cdb5c48860e674503a8ddb4
limpododo <mxxm994@gmail.com>
2023-06-01 01:15:13 +0300

    Добавлены свойства объекту VolumeDisk с помощью реализации интерфейса IDisk.

--- CloApi/Infrastructure/Storage/VolumeDisk.cs
+++ CloApi/Infrastructure/Storage/VolumeDisk.cs
@@ -1,12 +1,74 @@
-﻿using System;
+﻿using CloApi.Infrastructure.Network;
+using CloApi.Infrastructure.ServerObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.Storage
 {
-    public class VolumeDisk
+    public record class VolumeDisk : IDisk
     {
+        /// <summary>
+        ///  Объект содержащий информацию о сервере, к которому прикреплен диск
+        /// </summary>
+        [JsonPropertyName("attached_to_server")]
+        public Server? AttachedToServer { get; set; }
+
+        /// <summary>
+        /// Является ли диск загрузочным
+        /// </summary>
+        [JsonPropertyName("bootable")]
+        public bool? Bootable { get; set; }
+
+        /// <summary>
+        /// Дата создания диска
+        /// </summary>
+        [JsonPropertyName("created_in")]
+        public DateTime? DateCreated { get; set; }
+
+        /// <summary>
+        /// Уникальный идентификатор диска
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string? Id { get; set; }
+
+        /// <summary>
+        /// Массив ссылок на детальную информацию о диске
+        /// </summary>
+        [JsonPropertyName("links")]
+        public List<Link>? Links { get; set; }
+
+        /// <summary>
+        /// Имя диска
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Размер диска в гигабайтах
+        /// </summary>
+        [JsonPropertyName("size")]
+        public int? Size { get; set; }
+
+        /// <summary>
+        /// Имя устройства в операционной системе
+        /// </summary>
+        [JsonPropertyName("device")]
+        public string? Device { get; set; }
+
+        /// <summary>
+        /// Комментарий к диску
+        /// </summary>
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        /// <summary>
+        /// Является ли диск неотключаемым
+        /// </summary>
+        [JsonPropertyName("undetachable")]
+        public bool? Undetachable { get; set; }
     }
 }
