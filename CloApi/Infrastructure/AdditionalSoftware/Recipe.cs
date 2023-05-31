aa5e24a6a623fa068c221aa5b6b6af5995f52d2c
limpododo <mxxm994@gmail.com>
2023-05-31 22:49:04 +0300

    Добавлены свойста объекту Recipe

--- CloApi/Infrastructure/AdditionalSoftware/Recipe.cs
+++ CloApi/Infrastructure/AdditionalSoftware/Recipe.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using CloApi.Infrastructure.Project;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
 
 namespace CloApi.Infrastructure.AdditionalSoftware
 {
-    public class Recipe
+    public record class Recipe
     {
+        /// <summary>
+        /// Уникальный идентификатор рецепта
+        /// </summary>
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Минимально допустимый объём диска для установки рецепта
+        /// </summary>
+        [JsonPropertyName("min_disk")]
+        public int MinDisk { get; set; }
+
+        /// <summary>
+        /// Минимально допустимое количество RAM для установки рецепта
+        /// </summary>
+        [JsonPropertyName("min_ram")]
+        public int MinRam { get; set; }
+
+        /// <summary>
+        /// Минимально допустимое количество vCPU для установки рецепта
+        /// </summary>
+        [JsonPropertyName("min_vcpus")]
+        public int MinVCPUs { get; set; }
+
+        /// <summary>
+        /// Наименование рецепта
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Список, содержащий объекты образов ОС, с которыми возможна установка рецепта
+        /// </summary>
+        [JsonPropertyName("suitable_images")]
+        public List<Image> SuitableImages { get; set; }
+
     }
 }
