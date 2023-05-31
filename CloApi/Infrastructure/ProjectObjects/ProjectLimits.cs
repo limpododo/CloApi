525750df3b645082f822285a6bba5e90c8ca1921
limpododo <mxxm994@gmail.com>
2023-06-01 00:10:59 +0300

    Добавлены свойства объекту ProjectLimits

--- CloApi/Infrastructure/ProjectObjects/ProjectLimits.cs
+++ CloApi/Infrastructure/ProjectObjects/ProjectLimits.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.ProjectObjects
 {
-    public class ProjectLimits
+    public record class ProjectLimits
     {
+        /// <summary>
+        /// Верхнее ограничение по использованию ресурса
+        /// </summary>
+        [JsonPropertyName("max")]
+        public int Max { get; set; }
+
+        /// <summary>
+        /// Единица измерения
+        /// </summary>
+        [JsonPropertyName("measure")]
+        public string Measure { get; set; }
+
+        /// <summary>
+        /// Наименование ресурса
+        /// </summary>
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Количество использованного ресурса
+        /// </summary>
+        [JsonPropertyName("used")]
+        public int Used { get; set; }
     }
 }
