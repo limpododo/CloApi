4f907fad9ce8ae105c7a485b064ecb0699b326e0
limpododo <mxxm994@gmail.com>
2023-06-01 01:21:54 +0300

    Удаленны ненужные дерективы using

--- CloApi/Infrastructure/Network/FloatingIP.cs
+++ CloApi/Infrastructure/Network/FloatingIP.cs
@@ -1,10 +1,7 @@
 ﻿using CloApi.Infrastructure.ServerObjects;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.Network
 {
--- CloApi/Infrastructure/Storage/LocalDisk.cs
+++ CloApi/Infrastructure/Storage/LocalDisk.cs
@@ -2,10 +2,7 @@
 using CloApi.Infrastructure.ServerObjects;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.Storage
 {
