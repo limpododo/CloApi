bd30ea1f5ff240c2507d980e5c36b53ce3ddd3f6
limpododo <mxxm994@gmail.com>
2023-06-01 03:04:21 +0300

    Реализован метод DeleteAsync(.)

--- CloApi/API/Clo.cs
+++ CloApi/API/Clo.cs
@@ -14,6 +14,8 @@ namespace CloApi.API
 {
     public class Clo
     {
+        private static readonly string BASEURL = "https://api.clo.ru";
+
         private HttpClient _client;
         public Clo(string token)
         {
@@ -29,35 +31,45 @@ namespace CloApi.API
         #region ProjectAsyncMethods
         public async Task<List<Project>?> GetProjectsAsync()
         {
-            return await GetResultsAsync<Project>("https://api.clo.ru/v1/projects");
+            return await GetResultsAsync<Project>($"{BASEURL}/v1/projects");
         }
 
         public async Task<List<Image>?> GetImagesAsync(string projectId)
         {
-            return await GetResultsAsync<Image>($"https://api.clo.ru/v1/projects/{projectId}/images");
+            return await GetResultsAsync<Image>($"{BASEURL}/v1/projects/{projectId}/images");
         }
 
         public async Task<List<ProjectLimit>?> GetLimitsAsync(string projectId)
         {
-            return await GetResultsAsync<ProjectLimit>($"https://api.clo.ru/v1/projects/{projectId}/limits");
+            return await GetResultsAsync<ProjectLimit>($"{BASEURL}/v1/projects/{projectId}/limits");
         }
         #endregion
 
         #region RecipeAsyncMethods
         public async Task<List<Recipe>?> GetRecipeAsync()
         {
-            return await GetResultsAsync<Recipe>($"https://api.clo.ru/v1/recipe");
+            return await GetResultsAsync<Recipe>($"{BASEURL}/v1/recipe");
         }
         #endregion
 
         #region ObjectStorageS3AsyncMethods
         public async Task<List<S3User>?> GetS3UsersAsync(string projectId)
         {
-            return await GetResultsAsync<S3User>($"https://api.clo.ru/v1/projects/{projectId}/s3_users");
+            return await GetResultsAsync<S3User>($"{BASEURL}/v1/projects/{projectId}/s3_users");
+        }
+
+        public async Task<S3User> CreateS3User(string projectId, S3User user)
+        {
+            return await PostParametrsAsync<S3User>($"{BASEURL}/v1/projects/{projectId}/s3_users", user);
+        }
+
+        public async Task<S3User> CreateS3User(string projectId, S3User user)
+        {
+            return await PostParametrsAsync<S3User>($"{BASEURL}/v1/projects/{projectId}/s3_users", user);
         }
         #endregion
 
-        #region GetCloResponse
+        #region Get
         private async Task<List<T>> GetResultsAsync<T>(string path)
         {
             using (var message = await _client.GetAsync(path))
@@ -77,7 +89,7 @@ namespace CloApi.API
         }
         #endregion
 
-        #region PostCloResponse
+        #region Post
         private async Task<T> PostParametrsAsync<T>(string path, T obj)
         {
             using (var message = await _client.PostAsync(path, new StringContent(JsonSerializer.Serialize(obj))))
@@ -85,6 +97,13 @@ namespace CloApi.API
                 var cloResponse = await message.Content.ReadAsStringAsync();
                 return JsonSerializer.Deserialize<CloResponse<T>>(cloResponse).Result;
             }
+        }
+        #endregion
+
+        #region Delete
+        private async void DeleteAsync(string path)
+        {
+            await _client.DeleteAsync(path);
         }
         #endregion
     }
