d7fd7c75c00a3dc1690a903f24a43fd6eb4b4ca5
limpododo <mxxm994@gmail.com>
2023-06-01 03:16:37 +0300

    Добавлены свойства объекту Keys

--- CloApi/Infrastructure/ObjectStorageS3/Keys.cs
+++ CloApi/Infrastructure/ObjectStorageS3/Keys.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CloApi.Infrastructure.ObjectStorageS3
 {
-    public class Keys
+    public record class Keys
     {
+        /// <summary>
+        /// Уникальный идентификатор ключа пользователя S3
+        /// </summary>
+        [JsonPropertyName("access_key")]
+        public string? AccessKey { get; set; }
+
+        /// <summary>
+        /// Секретный ключ пользователя S3
+        /// </summary>
+        [JsonPropertyName("secret_key")]
+        public string? SecretKey { get; set; }
+
+        /// <summary>
+        /// Уникальный идентификатор пользователя в рамках хранилища S3
+        /// </summary>
+        [JsonPropertyName("user")]
+        public string? UserId { get; set; }
+
     }
 }
